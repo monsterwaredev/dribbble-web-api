c3c67d96209611af7b64d86f47eb939c720fde46
Samuel Ng <sam@infinitely.io>
2015-11-17 19:52:01 -0500

    add validations to shots route

--- src/routes/shots.js
+++ src/routes/shots.js
@@ -4,15 +4,20 @@ var Route = require('../route.js');
 module.exports = exports = [
     new Route([
         new Route.URI('get', '/shots'),
-        new Route.URI('get', '/shots/:itemPerPage'),
-        new Route.Response(function(req, res) {
+        new Route.URI('get', '/shots/:page', {
+            page: new Route.Validator(function(value, utility) {
+                return utility.isNumber(value) ? true : 'page value has to be a number';
+            })
+        }),
+        new Route.Response(function(err, req, res) {
+            console.log(err);
             res.end('hello world');
         })
     ]),
     new Route([
         new Route.URI('get', '/shot'),
         new Route.URI('get', '/shot/:itemPerPage'),
-        new Route.Response(function(req, res) {
+        new Route.Response(function(err, req, res) {
             res.end('hello world 2');
         })
     ])
